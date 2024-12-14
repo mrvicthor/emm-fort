4bfcc6a8421304082970956aca371f29bdacc781
Victor Eleanya <victoreleanya89@gmail.com>
2024-12-14 21:47:33 +0100

    fix(Signupform.tsx): amend mobile view

--- src/app/signup/page.tsx
+++ src/app/signup/page.tsx
@@ -3,7 +3,7 @@ import SignupForm from "./form";
 
 export default function Page() {
   return (
-    <section className="flex items-center h-screen w-screen justify-center gap-6 px-4">
+    <section className="flex items-center min-h-screen w-screen justify-center gap-6 px-4">
       <div className="bg-white w-[30rem] rounded-lg px-4 py-6 space-y-4">
         <h1 className="font-bold text-3xl capitalize">signup</h1>
         <p className="text-[#101010] opacity-40">Let&apos;s get you started</p>
