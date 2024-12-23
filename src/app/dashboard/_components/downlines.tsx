69360750fe001773c2a48e7f85cd092da4238ee2
Victor Eleanya <victoreleanya89@gmail.com>
2024-12-23 11:20:37 +0100

    feat(Downlines.tsx): added new component to the dashboard

---
+++ src/app/dashboard/_components/downlines.tsx
@@ -0,0 +1,43 @@
+import { formatCurrency } from "@/helpers";
+import React from "react";
+
+type Downlines = {
+  name: string;
+  tier: string;
+  benefits: number;
+};
+type DownlineProps = {
+  data: Downlines[];
+};
+const Downlines = ({ data }: DownlineProps) => {
+  return (
+    <div className="py-4 px-4 space-y-3 rounded-xl dashboard-shadow ">
+      <div className="flex justify-between items-center">
+        <h3 className="font-bold text-lg">Downlines</h3>
+        <h3 className="font-bold text-lg">Benefits</h3>
+      </div>
+      <ul className="flex flex-col gap-2">
+        {data.map((item, index) => (
+          <li
+            key={index}
+            className="flex justify-between items-center bg-[#F4F4F4] px-2 rounded-lg py-2"
+          >
+            <div>
+              <p className="font-bold">{item.name}</p>
+              <p className="text-sm">{item.tier}</p>
+            </div>
+            <div>{formatCurrency(item.benefits)}</div>
+          </li>
+        ))}
+      </ul>
+      <div className="flex justify-between items-center">
+        <button className="bg-[#ff5c00] text-white hover:opacity-40 py-2 px-4 rounded-xl font-bold">
+          Add down lines
+        </button>
+        <button className="font-bold">See more</button>
+      </div>
+    </div>
+  );
+};
+
+export default Downlines;
