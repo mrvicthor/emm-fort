b1b700059b6707a15fb33b5b5531586dfb9aa9fb
Victor Eleanya <victoreleanya89@gmail.com>
2024-12-27 17:53:58 +0100

    feat(dal.ts): added logic to get user details

--- src/app/lib/dal.ts
+++ src/app/lib/dal.ts
@@ -1,6 +1,8 @@
 import { cookies } from "next/headers";
 import { decrypt } from "../actions/session";
 import { redirect } from "next/navigation";
+import { cache } from "react";
+import User from "./schema";
 
 export async function verifySession() {
   const cookie = (await cookies()).get("session")?.value;
@@ -12,3 +14,19 @@ export async function verifySession() {
 
   return { isAuth: true, userId: session.userId };
 }
+
+export const getUser = cache(async () => {
+  const session = await verifySession();
+  if (!session) return null;
+
+  try {
+    const user = await User.findById(session.userId);
+    return {
+      ...user,
+      password: undefined,
+    };
+  } catch (error) {
+    console.log("Failed to fetch user: ", error);
+    return null;
+  }
+});
