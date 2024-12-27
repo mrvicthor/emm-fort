0e38c833412efe96b01dbfcd0939a82428321997
Victor Eleanya <victoreleanya89@gmail.com>
2024-12-27 17:49:18 +0100

    feat(dal.ts):  added logic to verify user

--- src/app/lib/dal.ts
+++ src/app/lib/dal.ts
@@ -1,7 +1,14 @@
 import { cookies } from "next/headers";
 import { decrypt } from "../actions/session";
+import { redirect } from "next/navigation";
 
 export async function verifySession() {
   const cookie = (await cookies()).get("session")?.value;
   const session = await decrypt(cookie);
+
+  if (!session?.userId) {
+    redirect("/login");
+  }
+
+  return { isAuth: true, userId: session.userId };
 }
