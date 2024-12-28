e1d4d586652f9d5f72b3f3bb69396bfb4180e7ed
Victor Eleanya <victoreleanya89@gmail.com>
2024-12-28 11:45:52 +0100

    feat(LoginForm.tsx): added login functionality to frontend

--- src/app/login/form.tsx
+++ src/app/login/form.tsx
@@ -1,9 +1,17 @@
+"use client";
+import React, { useActionState } from "react";
 import Link from "next/link";
-import React from "react";
+import { login } from "../actions/auth";
+import { LoginActionResponse } from "../lib/definitions";
 
+const initialState: LoginActionResponse = {
+  success: false,
+  message: "",
+};
 const LoginForm = () => {
+  const [state, action, pending] = useActionState(login, initialState);
   return (
-    <form className="space-y-4">
+    <form action={action} className="space-y-4">
       <div className="flex flex-col gap-2">
         <label htmlFor="email" className="">
           Email
@@ -13,19 +21,46 @@ const LoginForm = () => {
           name="email"
           type="text"
           placeholder="Enter email address"
-          className="border py-3 px-4 rounded-lg"
+          className={`border py-3 px-4 rounded-lg ${
+            state.errors?.email && "border-red-500"
+          }`}
+          required
         />
       </div>
+      {state.errors?.email && (
+        <p className="text-red-500">{state.errors.email}</p>
+      )}
       <div className="flex flex-col gap-2">
         <label htmlFor="password">Password</label>
         <input
           type="password"
           name="password"
           id="password"
-          className="border py-3 px-4 rounded-lg"
+          className={`border py-3 px-4 rounded-lg ${
+            state.errors?.password && "border-red-500"
+          }`}
           placeholder="Enter your password"
+          required
         />
       </div>
+      {state.errors?.password && (
+        <p className="text-red-500">{state.errors.password}</p>
+      )}
+
+      {state?.message && (
+        <div
+          className={`${
+            state.success ? "border-green-500" : "border-red-500"
+          } border py-3 px-4 rounded-lg `}
+        >
+          {state.success && (
+            <span className="material-symbols-outlined text-green-500 flex">
+              check_circle
+            </span>
+          )}
+          <p className="text-red-500">{state.message}</p>
+        </div>
+      )}
       <div className="text-right">
         <Link href="/forgot-password" className="text-[#ff5c00]">
           Forgot password?
@@ -35,7 +70,7 @@ const LoginForm = () => {
         type="submit"
         className="py-3 w-full bg-[#ff5c00] text-white capitalize rounded-lg hover:opacity-40"
       >
-        login
+        {pending ? "submiting..." : "login"}
       </button>
     </form>
   );
